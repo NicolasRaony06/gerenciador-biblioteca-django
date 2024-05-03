aea90fb3406dd0f30b9b04d7cccf3b26730f248a
unknown <nicolas.raony06@gmail.com>
2024-05-02 21:32:11 -0300

    adicionando arquivos base para a aplicação

---
+++ gerenciador_biblioteca/urls.py
@@ -0,0 +1,8 @@
+from django.contrib import admin
+from django.urls import path, include
+from django.shortcuts import render
+
+urlpatterns = [
+    path("admin/", admin.site.urls),
+    path("", lambda request: render(request, 'home.html')),
+]
